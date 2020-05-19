27d5054a192e05fe9b5268c563578d51c7d3aca8
Sammy Barasa <60486335+Sammy-Barasa@users.noreply.github.com>
2020-05-20 01:30:29 +0300

    Update api.py
    Retrieved the key

--- readerstation/api.py
+++ readerstation/api.py
@@ -3,7 +3,7 @@ import requests
 #API to Get review statistics given a list of ISBN
 def main():
         
-    #KEY="NxHR0wNCivzIEefBextQ"(you cannot represent keys)
-    #res = requests.get("https://www.goodreads.com/book/review_counts.json?isbns=1594489505&key=NxHR0wNCivzIEefBextQ")
-        res = requests.get("https://www.goodreads.com/book/review_counts.json",params={"isbns":1594489505,"key":"NxHR0wNCivzIEefBextQ"})
+    
+    #res = requests.get("https://www.goodreads.com/book/review_counts.json?isbns=1594489505&key=KEY")
+        res = requests.get("https://www.goodreads.com/book/review_counts.json",params={"isbns":1594489505,"key":KEY})
         a=print(res.json())
